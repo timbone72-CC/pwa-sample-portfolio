986235d62f01910b1027c31cb02be298c360545c
Tim Rush <timbone72@gmail.com>
2026-05-27 21:17:43 -0500

    Deploy Equipment Check Log demo to GitHub Pages docs

---
+++ docs/equipment-check-log/sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'equipment-check-log-v1'
+const BASE_PATH = '/pwa-sample-portfolio/equipment-check-log'
+
+const APP_SHELL = [
+  `${BASE_PATH}/`,
+  `${BASE_PATH}/index.html`,
+  `${BASE_PATH}/manifest.webmanifest`,
+  `${BASE_PATH}/favicon.svg`,
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)),
+  )
+
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((cacheName) => cacheName !== CACHE_NAME)
+            .map((cacheName) => caches.delete(cacheName)),
+        ),
+      ),
+  )
+
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse
+      }
+
+      return fetch(event.request)
+    }),
+  )
+})
